a650cde081b2f7ec62a23cca169b7822f1b119b3
77f315 <77f315@gmail.com>
2025-11-08 17:30:22 +0200

    register'

--- backend/src/models/user.ts
+++ backend/src/models/user.ts
@@ -1,11 +1,27 @@
-import { pool } from '../db';
-
+import { error } from "console";
+import { pool } from "../db";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 export interface User {
-    userId: number;
-    userName: string;
+  userId: number;
+  userName: string;
 }
 
 export async function getAllUsers(): Promise<User[]> {
-    const [rows] = await pool.query<User[]>('SELECT user_id, user_name from users');
-    return rows;
+  const [rows] = await pool.query<User[]>(
+    "SELECT user_id, user_name from users"
+  );
+  return rows;
 }
+
+export async function registerUser(userName: string, password: string) {
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  await pool.query("INSERT INTO users (user_name, password) values (?, ?)", [
+    userName,
+    hashedPassword,
+  ]);
+  return { message: "user registered" };
+}
+
--- backend/src/routes/users.ts
+++ backend/src/routes/users.ts
@@ -1,5 +1,6 @@
 import { FastifyInstance } from "fastify";
-import { getAllUsers } from "../models/user";
+import { getAllUsers, registerUser } from "../models/user";
+import { error } from "console";
 
 
 export default async function userRoutes(app:FastifyInstance) {
@@ -11,5 +12,20 @@ export default async function userRoutes(app:FastifyInstance) {
             app.log.error(err);
             reply.status(500).send({error: "failed to fetch users"});
         }
-    })
+    });
+
+    app.post("/register", async (request, reply) => {
+        try {
+            const {userName, password} = request.body as {userName: string; password: string};
+            if (!userName || !password){
+                return reply.status(400).send({error: `both username and password are
+                     required`});
+            }
+            const result = await registerUser(userName, password);
+            return result;
+        } catch (err: any) {
+            app.log.error(err);
+            reply.status(500).send({error: err.message});
+        }
+    });
 }
